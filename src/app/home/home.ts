bf89ef8d0d1204e74e02efcfb2ae1e4b41bd25ff
Luis Fernando Guarachi Apaza <luisfernandoguarachia@gmail.com>
2025-10-27 11:09:38 -0400

    feat: adicion de productoservice para consumir la api de backend

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,11 +1,37 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { Productoservice } from '../core/services/productoservice';
+
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
 export class Home {
+   //declaramos una variables para recibir la data del backend
+  productos:any[]=[];
+  /**
+   *
+   */
+  // creamo un constructor para llamar a nuestro servicio
+  constructor(private productoService:Productoservice) {
+        this.listarProductos();
+  }
+  //creamos un metodo para listar productos desde el servicio
+  listarProductos():void
+  {
+
+    this.productoService.listaProductos().subscribe({
+      next:(data)=> {
+        this.productos = data
+        console.log(this.productos);
+      
+      },
+      error:(err)=>console.error('error al cargar productos',err)
+    })
+
+  }
 
 }
